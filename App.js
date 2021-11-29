cda6bf7cd533bf5cf309c804779e3c7ea62a54a7
Urquhart <peyton.urquhart@wsu.edu>
2021-11-28 19:17:28 -0800

    add react-navigation

--- App.js
+++ App.js
@@ -2,18 +2,21 @@ import { StatusBar } from 'expo-status-bar';
 import { Navbar } from './components/Navbar.js';
 import { theme } from './style/theme.js';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
+    <NavigationContainer>
     <PaperProvider theme={theme}>
       <View style={styles.container}>
-      <Navbar/>
+        <Navbar/>
         <Text>App.js</Text>
         <StatusBar style="auto" />
       </View>
     </PaperProvider>
+    </NavigationContainer>
   );
 }
 
